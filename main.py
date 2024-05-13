4a4bc4b56fc1ee5b623df282a423a4ca8dcc91b8
Nitsh kumar <nitish10795@gmail.com>
2024-05-13 21:59:33 +0530

    requiremets added

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from src.textSummarizer.logging import logging
+
+
+# programe entry point
+if __name__ == "__main__":
+    logging.info (" the execution of programe started ")
+    print("main function")
